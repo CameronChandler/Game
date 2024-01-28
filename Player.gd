da5c596c6365115c6e7315f33a35ee57573a7804
CameronChandler <camchandler1@gmail.com>
2024-01-28 14:05:04 +1100

    Movement

--- Player.gd
+++ Player.gd
@@ -3,5 +3,16 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _physics_process(delta):
-
+	velocity = Vector2(0, 0)
+	var movement = Vector2(0, 0)
+	if Input.is_action_pressed("right"):
+		movement.x += 1
+	if Input.is_action_pressed("left"):
+		movement.x -= 1
+	if Input.is_action_pressed("up"):
+		movement.y -= 1
+	if Input.is_action_pressed("down"):
+		movement.y += 1
+	velocity += movement.normalized() * SPEED
+		
 	move_and_slide()
