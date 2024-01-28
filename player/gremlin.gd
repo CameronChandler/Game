c3b02565e475872117cc29c9d1954bd380d38eef
CameronChandler <camchandler1@gmail.com>
2024-01-28 16:48:52 +1100

    Gremlin following logic

--- player/gremlin.gd
+++ player/gremlin.gd
@@ -1,14 +1,41 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 200.0
+
+var TEAM
+var PLAYER
+var target
+
+var TEAM1_SPRITE = preload("res://images/player placeholder.png")
+var TEAM2_SPRITE = preload("res://images/player 2 placeholder.jpg")
+
+func _ready() -> void:
+	$Sprite2D.texture = TEAM1_SPRITE if TEAM == 1 else TEAM2_SPRITE
+	$TargetTimer.timeout.connect(_find_target)
+
+func _find_target() -> void:
+	var can_see = $View.get_overlapping_bodies()
+	if can_see.size() == 0:
+		target = PLAYER
+		return
+		
+	var closest_obj = PLAYER
+	var closest_dist = 10000.0
+	for other in can_see:
+		if other.TEAM != TEAM:
+			var distance = (other.global_position - global_position).length()
+			if distance < closest_dist:
+				closest_obj = other
+	
+	target = closest_obj
+	print(target)
 
 func _get_input() -> Vector2:
-	return Vector2(0, 0)
+	var direction = global_position.direction_to(target.global_position)
+	return direction * SPEED
 
-func _physics_process(delta):
-	var input = _get_input()
-	velocity = Vector2(0, 0)
-	var movement = input
-	velocity += movement.normalized() * SPEED
+func _physics_process(_delta):
+	var movement = _get_input()
+	velocity = movement.normalized() * SPEED
 		
 	move_and_slide()
--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,8 @@ class_name AbstractPlayer
 
 const SPEED = 300.0
 
+var TEAM: int = 0
+
 func _ready() -> void:
 	$GremlinTimer.timeout.connect(_spawn_gremlin)
 
@@ -15,10 +17,19 @@ func _physics_process(delta):
 	velocity = Vector2(0, 0)
 	var movement = input
 	velocity += movement.normalized() * SPEED
+	
+	if velocity.x < 0:
+		$Sprite2D.flip_h = true
+	elif velocity.x > 0:
+		$Sprite2D.flip_h = false
 		
 	move_and_slide()
 
 func _spawn_gremlin():
+	print('Spawn!')
 	var gremlin = preload("res://player/gremlin.tscn").instantiate()
+	gremlin.TEAM = TEAM
+	gremlin.target = self
+	gremlin.PLAYER = self
 	get_parent().add_child(gremlin)
 	gremlin.global_position = self.global_position
