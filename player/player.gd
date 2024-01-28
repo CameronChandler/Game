8c1362a7e3cf0d8e370008325b91efb8be733e53
James Chandler <Jam.Chandler@CCG.VIC.EDU.AU>
2024-01-28 16:50:28 +1100

    walls

--- player/player.gd
+++ player/player.gd
@@ -14,3 +14,17 @@ func _physics_process(delta):
 	velocity += movement.normalized() * SPEED
 		
 	move_and_slide()
+
+	if velocity.x < 0:
+		$Sprite2D.flip_h = true
+	elif velocity.x > 0:
+		$Sprite2D.flip_h = false
+
+func create_wall():
+	print('create wall')
+	var wall = preload("res://wall.tscn").instantiate()
+	wall.global_position = global_position + 100*velocity.normalized()
+	wall.rotation = atan2(velocity.y,velocity.x)
+	get_parent().add_child(wall)
+	
+
--- player/player2.gd
+++ player/player2.gd
@@ -2,6 +2,8 @@ extends AbstractPlayer
 
 func _get_input() -> Vector2:
 	var movement = Vector2(0, 0)
+	if Input.is_action_just_pressed("p2_wall"):
+		create_wall()
 	if Input.is_action_pressed("p2_right"):
 		movement.x += 1
 	if Input.is_action_pressed("p2_left"):
@@ -10,5 +12,5 @@ func _get_input() -> Vector2:
 		movement.y -= 1
 	if Input.is_action_pressed("p2_down"):
 		movement.y += 1
-		
+
 	return movement
